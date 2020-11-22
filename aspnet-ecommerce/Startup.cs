e347ac82a2ce2eff3fc4a06ef38b03ab16c76e4b
Sandy Andryanto <sandyandryanto@gmail.com>
2020-11-22 12:18:53 +0700

    first commit

---
+++ aspnet-ecommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet_ecommerce.Startup))]
+namespace aspnet_ecommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
